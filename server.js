0ab4d12500c807f44931bf0f439bce09958b2994
Mahipal Yadav <silenttalesoriginal@gmail.com>
2026-09-06 08:11:16 +0530

    first uploadd

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "public", "index.html")
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`VOID running on port ${PORT}`);
+});
