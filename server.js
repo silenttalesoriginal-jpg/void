4691304ab42b740063570a1c3fadf4428d5ede4a
silenttalesoriginal-jpg <silenttalesoriginal@gmail.com>
2026-09-25 16:31:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,31 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname, "public")));
+const publicPath = path.join(__dirname, "public");
+
+// Don't let browsers cache the main HTML or JavaScript files
+app.use((req, res, next) => {
+    if (
+        req.path === "/" ||
+        req.path.endsWith(".html") ||
+        req.path.endsWith(".js") ||
+        req.path.endsWith(".css")
+    ) {
+        res.setHeader(
+            "Cache-Control",
+            "no-cache, no-store, must-revalidate"
+        );
+    }
+
+    next();
+});
+
+app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "public", "index.html")
-    );
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(PORT, () => {
